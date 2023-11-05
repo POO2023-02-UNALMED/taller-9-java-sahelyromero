fe88bdd33642341e5d31f7be7de906c20df09cf5
Sahely Romero <141962337+sahelyromero@users.noreply.github.com>
2023-11-05 13:48:19 -0500

    Calculadora arreglada

--- src/main/java/gui/Calculadora.java
+++ src/main/java/gui/Calculadora.java
@@ -1,25 +1,25 @@
-package gui;
+package application;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-public class Calculadora extends Application  {
+public class Calculadora extends Application{
 	
 	public static void main(String args[]){ 
-	      ***(args); 
+	      launch(args); 
 	} 
 	
 	@Override
 	public void start(Stage window) throws Exception {
-		*** vb = new VBox();
-		window.setTitle(***);
+		VBox vb = new VBox();
+		window.setTitle("Calculadora");
 		
 		vb.getChildren().add(new Calculator());
-		Scene sc = new Scene(***, 270, 270);
-		window.setScene(***);
-		***.show();
+		Scene sc = new Scene(vb, 270, 270);
+		window.setScene(sc);
+		window.show();
 	}
 	
 }
